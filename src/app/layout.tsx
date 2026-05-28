b57d316d736e5cafdc81d6d1f23d0787b9789c98
shefcodev <sherefjimoh@gmail.com>
2026-05-28 01:15:59 +0100

    added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="antialiased">
+<body className="antialiased">
         <Navigation />
         <main>{children}</main>
         <Footer />
